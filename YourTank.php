9dec93921d1c54e5f00a1f340c9d585a7e4b2529
Yi-YaoWang <37430936+Yi-YaoWang@users.noreply.github.com>
2018-12-28 21:27:43 +0800

    Add files via upload

---
+++ YourTank.php
@@ -0,0 +1,89 @@
+<html>
+	<head>
+    <style>
+    #Tanks{
+    background-image:url('<?php echo $_POST["Tanks"]; ?>');
+    background-repeat:no-repeat;
+    background-color:#2B2B2B;
+    background-attachment:fixed;
+    width:1920px;
+    height:1080px;
+        }
+    </style>
+	</head>
+	<body bgcolor="#2B2B2B">
+    <div id="Tanks">
+		<div id ="fishes_1"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes1"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_2"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes2"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_3"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes3"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_4"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes4"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_5"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes5"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_5"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes5"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_6"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes6"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_7"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes7"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_8"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes8"]; ?>" width="175px" height="100px" />
+		</div>
+		<div id ="fishes_9"style="position: absolute; 
+			top: <?php echo rand(0, 500); ?>px; 
+			bottom: <?php echo rand(0, 500); ?>px; 
+			left: <?php echo rand(0, 500); ?>px; 
+			right: <?php echo rand(0, 500); ?>px; "> 
+			<img src="<?php echo $_POST["fishes9"]; ?>" width="175px" height="100px" />
+		</div>
+		
+    </div>
+	</body>
+</html>
